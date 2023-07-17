25347a104b081d56ab118458c48145f50ee6f9fe
Jony-Jas <jonyjasjonyjas@gmail.com>
2023-07-17 18:53:16 +0530

    rest api

---
+++ src/main/java/com/jonyjas/movies/MovieService.java
@@ -0,0 +1,27 @@
+package com.jonyjas.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    private MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
